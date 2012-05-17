4a34a29b3b31c595db3da1c84ad82e020879b5ea
Tomek Zdyba? <tomek@zdybal.lap.pl>
2012-05-17 10:49:57 +0200

    initial Eclipse project

---
+++ src/org/trektrace/TreckTraceScreen.java
@@ -0,0 +1,19 @@
+package org.trektrace;
+
+import net.rim.device.api.ui.container.MainScreen;
+
+/**
+ * A class extending the MainScreen class, which provides default standard
+ * behavior for BlackBerry GUI applications.
+ */
+public final class TreckTraceScreen extends MainScreen
+{
+    /**
+     * Creates a new TreckTraceScreen object
+     */
+    public TreckTraceScreen()
+    {        
+        // Set the displayed title of the screen       
+        setTitle("TrekTrace");
+    }
+}
