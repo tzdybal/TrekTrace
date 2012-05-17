6a08e5d8a4e541863c39697104af82f25f5e71e8
Tomek Zdyba? <tomek@zdybal.lap.pl>
2012-05-17 17:38:53 +0200

    database development

--- src/org/trektrace/db/DatabaseManager.java
+++ src/org/trektrace/db/DatabaseManager.java
@@ -6,6 +6,7 @@ import net.rim.device.api.database.Database;
 import net.rim.device.api.database.DatabaseFactory;
 import net.rim.device.api.database.DatabaseIOException;
 import net.rim.device.api.database.DatabasePathException;
+import net.rim.device.api.database.Statement;
 import net.rim.device.api.io.MalformedURIException;
 import net.rim.device.api.io.URI;
 
@@ -29,7 +30,6 @@ public class DatabaseManager {
 		} else {
 			try {
 				db = DatabaseFactory.create(DB_URI);
-				createTables();
 				return true;
 			} catch (Exception e) {
 				throw new DatabaseException(e);
@@ -43,7 +43,7 @@ public class DatabaseManager {
 		}
 		return db;
 	}
-	
+
 	public static boolean databaseExists() {
 		try {
 			if (DB_URI == null)
@@ -55,18 +55,57 @@ public class DatabaseManager {
 		}
 	}
 
-
 	private static void openDatabase() throws DatabaseException {
 		try {
 			URI uri = URI.create(Configuration.DB_FILE);
 			db = DatabaseFactory.open(uri);
+			createTables();
 		} catch (Exception e) {
 			throw new DatabaseException(e);
 		}
 	}
-	
-	private static void createTables() {
-		
-		
+
+	private static void createTables() throws DatabaseException {
+		try {
+			Statement pointsStmt = db.createStatement("CREATE TABLE IF NOT EXISTS points" +
+					"(" +
+					"id INTEGER PRIMARY KEY" +
+					", route_id INTEGER NOT NULL" +
+					", altitude DOUBLE NOT NULL" +
+					", latitude DOUBLE NOT NULL" +
+					", longitude DOUBLE NOT NULL" +
+					", date INTEGER NOT NULL" +
+					")");
+			
+			Statement routesStmt = db.createStatement("CREATE TABLE IF NOT EXISTS routes " +
+					"(" +
+					"id INTEGER PRIMARY KEY" +
+					", name TEXT NOT NULL" +
+					", description TEXT" +
+					")");
+			
+			Statement settingsStmt = db.createStatement("CREATE TABE IF NOT EXISTS settings " +
+					"(" +
+					"key TEXT NOT NULL" +
+					", value TEXT" +
+					")");
+			
+			pointsStmt.prepare();
+			routesStmt.prepare();
+			settingsStmt.prepare();
+			
+			db.beginTransaction();
+			pointsStmt.execute();
+			routesStmt.execute();
+			settingsStmt.execute();
+			db.commitTransaction();
+			
+			pointsStmt.close();
+			routesStmt.close();
+			settingsStmt.close();
+		} catch (Exception e) {
+			throw new DatabaseException(e);
+		}
+
 	}
 }
