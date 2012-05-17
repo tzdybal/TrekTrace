710d23bc38af5637e2d15876968991379b437a29
tzdybal <tomek@zdybal.lap.pl>
2012-05-17 10:49:57 +0200

    initial Eclipse project

---
+++ src/org/trektrace/TreckTraceApp.java
@@ -0,0 +1,32 @@
+package org.trektrace;
+
+import net.rim.device.api.ui.UiApplication;
+
+/**
+ * This class extends the UiApplication class, providing a
+ * graphical user interface.
+ */
+public class TreckTraceApp extends UiApplication
+{
+    /**
+     * Entry point for application
+     * @param args Command line arguments (not used)
+     */ 
+    public static void main(String[] args)
+    {
+        // Create a new instance of the application and make the currently
+        // running thread the application's event dispatch thread.
+        TreckTraceApp theApp = new TreckTraceApp();       
+        theApp.enterEventDispatcher();
+    }
+    
+
+    /**
+     * Creates a new TreckTraceApp object
+     */
+    public TreckTraceApp()
+    {        
+        // Push a screen onto the UI stack for rendering.
+        pushScreen(new TreckTraceScreen());
+    }    
+}
